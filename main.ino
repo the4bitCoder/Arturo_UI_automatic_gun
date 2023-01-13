d5da3d2894cc2dd6c93e8cb8bc1dab7074e5c706
the4bitCoder <120564285+the4bitCoder@users.noreply.github.com>
2023-01-13 17:21:14 +0000

    Update main.ino

--- main.ino
+++ main.ino
@@ -1,1 +1,22 @@
+#include <SPI.h>
+#include <SdFat.h>
+#include <SdFatUtil.h>
+#include <SFEMP3Shield.h>
 
+int buttonPinOne = ;
+int buttonPinTwo = ;
+int buttonPinThree = ;
+int buttonPinFour = ;
+int helddown = ;
+
+SFEMP3Shield MP3player ;
+
+void setup()
+{
+  
+}
+
+void loop()
+{
+  
+}
