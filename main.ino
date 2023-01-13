739562d11c362af57bb95c61481564b22f0617ab
the4bitCoder <120564285+the4bitCoder@users.noreply.github.com>
2023-01-13 17:25:16 +0000

    Update main.ino

--- main.ino
+++ main.ino
@@ -3,17 +3,23 @@
 #include <SdFatUtil.h>
 #include <SFEMP3Shield.h>
 
-int buttonPinOne = ;
-int buttonPinTwo = ;
-int buttonPinThree = ;
-int buttonPinFour = ;
-int helddown = ;
+int buttonPinOne = 0 ;
+int buttonPinTwo = 0 ;
+int buttonPinThree = 0 ;
+int buttonPinFour = 0 ;
+int helddown = 0 ;
 
 SFEMP3Shield MP3player ;
 
 void setup()
 {
-  
+  MP3player.begin();
+  MP3player.SetVolume(0,0);
+  pinMode(A0,INPUT);
+  pinMode(A1,INPUT);
+  pinMode(A2,INPUT);
+  pinMode(A3,INPUT);
+  pinMode(A4,INPUT);
 }
 
 void loop()
